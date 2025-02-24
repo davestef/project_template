a425aad3d9605adfb5affcfa445fd9dcfd6fe021
davestef <79332500+davestef@users.noreply.github.com>
2025-02-24 08:17:45 -0500

    Add files via upload

---
+++ proj_structure_script.bat
@@ -0,0 +1,16 @@
+::================================================
+:: This batch script creates the standard
+:: directory structure for data science projects
+:: and creates a virtual python dev environment.
+::================================================
+mkdir data
+mkdir data\raw
+mkdir data\processed
+mkdir docs
+mkdir src
+mkdir utils
+mkdir notebooks
+mkdir config
+mkdir models
+:: python -m venv .venv-dev
+dir
